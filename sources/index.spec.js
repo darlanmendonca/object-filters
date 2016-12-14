f89f987fcee0fd1fdef00539520796a1d30f446f
Darlan Mendonça <darlanmendonca@gmail.com>
2016-12-14 21:15:11 -0200

    add specs defined in readme, disabled

--- sources/index.spec.js
+++ sources/index.spec.js
@@ -1,14 +1,54 @@
 import filters from './index.js'
 import {expect} from 'chai'
 
+const object = {
+  firstname: 'John',
+  lastname: 'Snow',
+  email: 'iknownothing@snow.com',
+  address: {
+    castle: 'Black',
+    region: 'North',
+  },
+}
+
 describe('object-filters', () => {
   it('module export a function', () => {
     expect(filters).to.be.an('function')
   })
 
   it('objects has a prototype method filter', () => {
-    expect({})
+    expect(object)
       .to.have.property('filters')
       .to.be.an('function')
+  })
+
+  xit('filter by strings separated by space', () => {
+    const {firstname, lastname} = object
+    expect(object.filters('firstname lastname'))
+      .to.deep.equal({firstname, lastname})
+  })
+
+  xit('filter by array of strings', () => {
+    const {firstname, lastname} = object
+    expect(object.filters(['firstname', 'lastname']))
+      .to.deep.equal({firstname, lastname})
+  })
+
+  xit('filter by negative strings separated by space', () => {
+    const {firstname, lastname} = object
+    expect(object.filters('-email -address'))
+      .to.deep.equal({firstname, lastname})
+  })
+
+  xit('filter by negative strings array', () => {
+    const {firstname, lastname} = object
+    expect(object.filters('-email -address'))
+      .to.deep.equal({firstname, lastname})
+  })
+
+  xit('filter by nested object', () => {
+    const {firstname, lastname} = object
+    expect(object.filters('firstname lastname address.castle'))
+      .to.deep.equal({firstname, lastname, address: {castle: 'Black'}})
   })
 })
