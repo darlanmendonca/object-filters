f759a3d019067f9cbb2891ec08d16c14ca216121
Darlan Mendonça <darlanmendonca@gmail.com>
2016-12-15 10:49:29 -0200

    split keys to array

--- sources/index.js
+++ sources/index.js
@@ -2,7 +2,24 @@ import clone from 'clone'
 
 module.exports = filters
 
-function filters() {
+function filters(options) {
+  const keys = splitKeys(options)
+  const negativeKeys = keys.filter(key => key.startsWith('-'))
+
+  console.log(keys, negativeKeys)
+
   const obj = clone(this)
   return obj
+}
+
+function splitKeys(options='') {
+  const keys = Array.isArray(options)
+    ? options.join(' ')
+    : options.toString()
+
+  return keys
+    .trim()
+    .replace(/\s{2,}/, ' ')
+    .split(' ')
+    .filter(key => key)
 }
